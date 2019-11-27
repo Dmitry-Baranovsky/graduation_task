321fc568d510e9aba183b7cbc92b6bc29c40b340
ihorklymenko <ihor@iteriodata.com>
2019-11-27 11:38:06 +0200

    Initial commit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,10 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0e16cbe94cf58db11"
+  ami           = "ami-091e3fae178527fb8"
   instance_type = "t2.micro"
   key_name      = "graduation_work"
   tags = {
     Name = "Jenkins"
+  }
+}
+
+resource "aws_instance" "docker2" {
+  ami           = "ami-0ee22ce00800c268d"
+  instance_type = "t2.micro"
+  key_name      = "graduation_work"
+  tags = {
+    Name = "Docker2"
+  }
+}
+
+resource "aws_instance" "docker1" {
+  ami           = "ami-0ee22ce00800c268d"
+  instance_type = "t2.micro"
+  key_name      = "graduation_work"
+  tags = {
+    Name = "Docker1"
   }
 }
